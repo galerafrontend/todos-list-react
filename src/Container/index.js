4e388a039e49ad1025d6c3cc9a5b7967fd881808
Karol Galera <frontend.galera@gmail.com>
2022-08-07 15:46:02 +0200

    styledComponents added to Container component

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,9 +1,9 @@
-import "./style.css";
+import { Main } from "./styled";
 
-const Container = ({children}) => (
-    <main className="main">
-        {children}
-    </main>
+const Container = ({ children }) => (
+	<Main>
+		{children}
+	</Main>
 );
 
 export default Container;
