f05d567a61d5b0e6947a6bdf80186f45b241d1bb
Karol Galera <frontend.galera@gmail.com>
2022-08-28 17:48:17 +0200

    application split into features

--- src/App.js
+++ src/App.js
@@ -1,54 +1,5 @@
-import Form from "./Form";
-import List from "./List";
-import Buttons from "./Buttons";
-import Section from "./Section";
-import Header from "./Header";
-import { Container } from "./styled";
-import { useState } from "react";
-import { useTasks } from "./useTasks";
+import Tasks from "./features/tasks/Tasks";
 
-function App() {
-  const [hideDoneTasks, setHideDoneTasks] = useState(false);
-
-  const toggleHideDone = () => {
-    setHideDoneTasks(hideDoneTasks => !hideDoneTasks);
-  };
-
-  const {
-    tasks,
-    removeTask,
-    toggleTaskDone,
-    markAllDone,
-    addNewTask
-  } = useTasks();
-
-  return (
-    <Container>
-      <Header title="Lista zadań" />
-      <Section
-        title="Dodaj nowe zadanie"
-        body={<Form addNewTask={addNewTask} />}
-      />
-      <Section
-        title="Lista zadań"
-        body={
-          <List
-            tasks={tasks}
-            hideDoneTasks={hideDoneTasks}
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone}
-          />}
-        extraHeaderContent={
-          <Buttons
-            tasks={tasks}
-            hideDoneTasks={hideDoneTasks}
-            toggleHideDone={toggleHideDone}
-            markAllDone={markAllDone}
-          />
-        }
-      />
-    </Container>
-  );
-}
+const App = () => <Tasks />;
 
 export default App;
---
+++ src/features/tasks/Tasks.js
@@ -0,0 +1,54 @@
+import Form from "./Form";
+import List from "./List";
+import Buttons from "./Buttons";
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+import { Container } from "./styled";
+import { useState } from "react";
+import { useTasks } from "../../useTasks";
+
+function App() {
+  const [hideDoneTasks, setHideDoneTasks] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDoneTasks(hideDoneTasks => !hideDoneTasks);
+  };
+
+  const {
+    tasks,
+    removeTask,
+    toggleTaskDone,
+    markAllDone,
+    addNewTask
+  } = useTasks();
+
+  return (
+    <Container>
+      <Header title="Lista zadań" />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
+      />
+      <Section
+        title="Lista zadań"
+        body={
+          <List
+            tasks={tasks}
+            hideDoneTasks={hideDoneTasks}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />}
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDoneTasks={hideDoneTasks}
+            toggleHideDone={toggleHideDone}
+            markAllDone={markAllDone}
+          />
+        }
+      />
+    </Container>
+  );
+}
+
+export default App;
