83c6f1e397238fdbeb60b78ed4c59047d68f591b
Karol Galera <frontend.galera@gmail.com>
2022-08-28 21:20:53 +0200

    customHook useTasks removed

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -4,18 +4,8 @@ import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import { Container } from "./styled";
-import { useTasks } from "../../useTasks";
 
 function App() {
-
-  const {
-    //tasks,
-    removeTask,
-    toggleTaskDone,
-    markAllDone,
-    addNewTask
-  } = useTasks();
-
   return (
     <Container>
       <Header title="Lista zadań" />
