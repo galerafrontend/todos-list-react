ece2acb2b8af83e3cf6eb9286b52a751731643fc
Karol Galera <frontend.galera@gmail.com>
2022-09-05 22:47:37 +0200

    new component added and search engine added

---
+++ src/features/tasks/TasksPage/Search/index.js
@@ -0,0 +1,28 @@
+import { Input } from "../Form/styled";
+import searchQueryParamName from "../searchQueryParamName";
+import { useQueryParameter, useReplaceQueryParameter } from "./queryParameters";
+import { Wrapper } from "./styled";
+
+const Search = () => {
+  const query = useQueryParameter(searchQueryParamName);
+  const replaceQueryParameter = useReplaceQueryParameter();
+
+  const onInputChange = ({ target }) => {
+    replaceQueryParameter({
+      key: searchQueryParamName,
+      value: target.value.trim() !== "" ? target.value : undefined,
+    })
+  };
+
+  return (
+    <Wrapper>
+      <Input
+        placeholder="Filtruj zadania"
+        value={query || ""}
+        onChange={onInputChange}
+      />
+    </Wrapper>
+  )
+};
+
+export default Search;
--- src/features/tasks/TasksPage/index.js
+++ src/features/tasks/TasksPage/index.js
@@ -4,6 +4,7 @@ import Buttons from "./Buttons";
 import SpecialButton from "./SpecialButton";
 import Section from "../../../common/Section";
 import Header from "../../../common/Header";
+import Search from "./Search";
 import { Container } from "../styled";
 
 function TasksPage() {
@@ -14,6 +15,10 @@ function TasksPage() {
         title="Dodaj nowe zadanie"
         body={<Form />}
         extraHeaderContent={<SpecialButton />}
+      />
+      <Section
+        title="Szukaj zadania"
+        body={<Search />}
       />
       <Section
         title="Lista zadań"
