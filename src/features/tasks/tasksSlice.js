ec12ff5ff98562682bc0a2ddc13c6ecd3d6ff075
Karol Galera <frontend.galera@gmail.com>
2022-09-05 21:45:59 +0200

    new selector added

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -60,7 +60,17 @@ export const selectAreTasksEmpty = state => selectTasks(state).length === 0;
 export const selectIsEveryTaskDone = state => selectTasks(state).every(({ done }) => done);
 export const selectIsAnyTaskDone = state => selectTasks(state).every(({ done }) => !done);
 
-export const getTaskById = (state, taskid) =>
-  selectTasks(state).find(({ id }) => id === taskid);
+export const getTaskById = (state, taskId) =>
+  selectTasks(state).find(({ id }) => id === taskId);
 
+export const selectTasksByQuery = (state, query) => {
+  const tasks = selectTasks(state);
+
+  if (!query || query.trim() === "") {
+    return tasks;
+  }
+
+  return tasks.filter(({ content }) => content.toUpperCase().includes(query.trim().toUpperCase()));
+};
+  
 export default tasksSlice.reducer;
